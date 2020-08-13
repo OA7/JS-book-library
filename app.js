13d9126ab0ecac33b5ac779793a1e7ac6b91f3d8
adam <aabou9@gmail.com>
2020-08-13 16:40:31 +0100

    add read btn

--- app.js
+++ app.js
@@ -9,20 +9,30 @@ function Book(title, author, pages, read = false) {
   this.read = read;
 }
 
+function chkbtn(val, status){
+  if (val) {
+    return `<button id='unread'> ${status} </button>`
+  } else {
+    return `<button id='read'> ${status} </button>`
+  }
+}
+
 // Function to add books to Array
 function addBookToLibrary(book) {
   const list = document.querySelector('#book-table');
   const row = document.createElement('tr');
-  let status = book.read === false ? 'Please read book' : 'Good job';
+  let status = book.read ? 'Read' : 'Unread';
 
   row.innerHTML = `
     <td>${book.title}</td>
     <td>${book.author}</td>
     <td>${book.pages}</td>
-    <td>${status}</td>
+    <td>${chkbtn(book.read, status)}</td>
   `
   list.appendChild(row);
 }
+
+// console.log(document.querySelector('.messageCheckbox:checked').value);
 
 // Adding book from form
 document.querySelector('#book-form').addEventListener('submit', (e) => {
@@ -30,8 +40,9 @@ document.querySelector('#book-form').addEventListener('submit', (e) => {
   const title = document.getElementById('title').value;
   const author = document.getElementById('author').value;
   const pages = document.getElementById('pages').value;
-
-  const book = new Book(title, author, pages);
+  const read = document.getElementById('checkbox').checked;
+  // console.log(read);
+  const book = new Book(title, author, pages, read);
   addBookToLibrary(book);
   myLibrary.push(book);
   console.log(myLibrary);

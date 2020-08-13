2aacf987c1985edb41a7827748cddfa8e7eeecf6
adam <aabou9@gmail.com>
2020-08-13 15:01:17 +0100

    add addBookToLibrary

--- app.js
+++ app.js
@@ -1,12 +1,17 @@
 let myLibrary = [];
 
 function Book(title, author, pages, read) {
-  this.title = title
-  this.author = author
-  this.pages = pages
-  this.read = read
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.read = read;
 }
 
-function addBookToLibrary() {
-  
+let firstBook = new Book('artof', 'me', '222', true);
+function addBookToLibrary(book) {
+  myLibrary.push(book);
 }
+
+// addBookToLibrary(firstBook);
+
+// console.log(myLibrary);

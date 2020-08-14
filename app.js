35a8c32184c07a34a3a80327e453295c9a6a4fba
adam <aabou9@gmail.com>
2020-08-14 13:33:21 +0100

    add render data from array

--- app.js
+++ app.js
@@ -9,30 +9,42 @@ function Book(title, author, pages, read = false) {
   this.read = read;
 }
 
-function chkbtn(val, status){
-  if (val) {
-    return `<button id='unread'> ${status} </button>`
-  } else {
-    return `<button id='read'> ${status} </button>`
-  }
+function chkbtn(val, i){
+  let status = val ? 'Read' : 'Unread';
+    return `<button class='readbtn' onclick="toggleRead(${i})" id='readbtn'> ${status} </button>`
+
 }
+
+
+// console.log(document.querySelector('.messageCheckbox:checked').value);
 
 // Function to add books to Array
 function addBookToLibrary(book) {
-  const list = document.querySelector('#book-table');
-  const row = document.createElement('tr');
-  let status = book.read ? 'Read' : 'Unread';
+  // const row = document.createElement('tr');
+  myLibrary.push(book);
 
-  row.innerHTML = `
-    <td>${book.title}</td>
-    <td>${book.author}</td>
-    <td>${book.pages}</td>
-    <td>${chkbtn(book.read, status)}</td>
-  `
-  list.appendChild(row);
 }
 
-// console.log(document.querySelector('.messageCheckbox:checked').value);
+
+
+// Render view
+function render() {
+  // const list = document.querySelector('#book-table').innerHTML('');
+  document.getElementById('book-table').innerHTML = '';
+  
+  for(let i=0;myLibrary.length>i;i++){
+    const row = document.createElement('tr');
+    const list = document.querySelector('#book-table');
+    console.log(i);
+    row.innerHTML = `
+    <td>${myLibrary[i].title}</td>
+    <td>${myLibrary[i].author}</td>
+    <td>${myLibrary[i].pages}</td>
+    <td>${chkbtn(myLibrary[i].read, i)}</td>
+    `;
+    list.appendChild(row);
+  }
+}
 
 // Adding book from form
 document.querySelector('#book-form').addEventListener('submit', (e) => {
@@ -44,16 +56,17 @@ document.querySelector('#book-form').addEventListener('submit', (e) => {
   // console.log(read);
   const book = new Book(title, author, pages, read);
   addBookToLibrary(book);
-  myLibrary.push(book);
   console.log(myLibrary);
+
+  render();
 });
 
-
-// Render view
-function render() {
-
+function toggleRead(index) {
+  myLibrary[index].read = !myLibrary[index].read;
+  // document.getElementById('#book-form').load();
+  render();
 }
-
+// var x = document.getElementsByClassName("readbtn");
 // Validation for input
-
+ 
 //
